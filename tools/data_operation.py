3d0b820a773ec84bde60be1c98286d8c75d3bb95
Yutong Liu <yutong@Yutongs-MacBook-Pro.local>
2018-09-15 14:45:46 -0700

    Add File: data_opeations

---
+++ tools/data_operation.py
@@ -0,0 +1,54 @@
+from __future__ import division
+
+
+import sys
+import math
+import numpy as np
+
+
+
+
+def calculate_entropy(y):
+    'Return the entroy of label y'
+    pass
+
+
+def mean_square_error(y_true, y_pred):
+    'Return mean squared error between prediction and true value'
+    pass
+
+
+def calculate_variance(y):
+    'Return variance of the features X'
+    pass
+    
+
+def calculate_std_dev(y):
+    'Return standard_deviation of features X'
+    pass
+
+
+def euclidean_distance(v1, v2):
+    'Return l2 distance between 2 vectors'
+    pass
+
+
+def accuracy_score(y_true, y_pred):
+    'Return accuracy after comparing prediction and true value'
+    pass
+
+
+def calculate_covariance_matrix(D, M=None):
+    'Return covariance matrix for dataset D'
+    pass
+
+
+def calculate_correlation_matrix(D, M=None):
+    'Return correlation matrix for dataset D'
+    pass
+
+
+
+
+
+
